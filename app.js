806a2dcea826990a07ad39827caa201b8c4afec7
See-Monkey <226230446+See-Monkey@users.noreply.github.com>
2026-02-26 21:43:37 -0700

    Install jest and supertest

--- app.js
+++ app.js
@@ -48,8 +48,4 @@ app.use((req, res) => {
 // Catch middleware errors
 app.use(errorHandler);
 
-// Server
-const PORT = process.env.PORT || 3000;
-app.listen(PORT, () => {
-	console.log(`Server running on http://localhost:${PORT}`);
-});
+export default app;
